892f6e6958a4234447fed12d469cd3a0c479f140
hexi1997 <18715156450@163.com>
2023-07-01 15:23:12 +0800

    refactor: footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,13 +1,14 @@
 import Link from "next/link";
 
 export function Footer() {
-    return  <footer className="p-10 footer text-neutral-content">
-    <div>
-      <p>Cyber Fortune Teller</p>
-    </div>
-    <div>
-      <div className="grid grid-flow-col gap-4">
-        <Link href="https://twitter.com/L_x_x_x_x_x">
+  return (
+    <footer className="p-10 text-center text-neutral-content">
+      {/* TODO: 这行和内容区内容重复 */}
+      {/* <div>
+        <p>Cyber Fortune Teller</p>
+      </div> */}
+      <div className="flex items-center justify-center gap-x-[16px] mt-[16px]">
+        <Link target="__blank" href="https://twitter.com/L_x_x_x_x_x">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="24"
@@ -18,7 +19,7 @@ export function Footer() {
             <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
           </svg>
         </Link>
-        <Link href="https://github.com/lyleshaw/TweetAnalyzer">
+        <Link target="__blank" href="https://github.com/lyleshaw/TweetAnalyzer">
           <svg
             viewBox="0 0 16 16"
             className="w-5 h-5"
@@ -29,6 +30,6 @@ export function Footer() {
           </svg>
         </Link>
       </div>
-    </div>
-  </footer>
+    </footer>
+  );
 }
